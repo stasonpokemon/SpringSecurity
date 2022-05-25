7963828798477336e314e865bdc4a434b6d755b1
Stanislau Trebnikau <Stanislau_Trebnikau@epam.com>
2022-05-25 17:07:57 +0300

    add database, add mysql-connector and c3p0 dependencies, configuration continuation,

--- src/main/java/com/trebnikau/configuration/MyConfig.java
+++ src/main/java/com/trebnikau/configuration/MyConfig.java
@@ -1,11 +1,15 @@
 package com.trebnikau.configuration;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 @Configuration
 @ComponentScan(basePackages = "com.trebnikau")
@@ -21,4 +25,18 @@ public class MyConfig {
         return internalResourceViewResolver;
     }
 
+    // Бин отвечающий за конект с базой данных
+    @Bean
+    public DataSource dataSource(){
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false");
+            dataSource.setUser("spring");
+            dataSource.setPassword("admin");
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
+    }
 }
--- src/main/java/com/trebnikau/configuration/MySecurityConfig.java
+++ src/main/java/com/trebnikau/configuration/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package com.trebnikau.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,25 +8,32 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity // Аннотация уже является конфигурацией
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication().
-                withUser(userBuilder.
-                        username("anton").
-                        password("anton").
-                        roles("EMPLOYEE")).
-                withUser(userBuilder.
-                        username("elena").
-                        password("elena").
-                        roles("HR")).
-                withUser(userBuilder.
-                        username("ivan").
-                        password("ivan").
-                        roles("MANAGER","HR"));
+//        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//        auth.inMemoryAuthentication().
+//                withUser(userBuilder.
+//                        username("anton").
+//                        password("anton").
+//                        roles("EMPLOYEE")).
+//                withUser(userBuilder.
+//                        username("elena").
+//                        password("elena").
+//                        roles("HR")).
+//                withUser(userBuilder.
+//                        username("ivan").
+//                        password("ivan").
+//                        roles("MANAGER", "HR"));
+        // Указываем конфигурации, что информация о user-ах находится в бд
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 
     // Метод для разрешение доступа к URL для конкретных ролей
@@ -34,11 +42,11 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().
                 antMatchers("/").hasAnyRole(
                         "EMPLOYEE",
-                                "HR",
-                                "MANAGER").
+                        "HR",
+                        "MANAGER").
                 antMatchers("/hr_info").hasRole("HR").
                 antMatchers("/manager_info").hasRole("MANAGER").
                 // Форма логина и пароля будет запрашиваться у всех
-                and().formLogin().permitAll();
+                        and().formLogin().permitAll();
     }
 }
