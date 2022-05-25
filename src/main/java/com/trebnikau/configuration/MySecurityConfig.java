740e0cd1ba6c65c96d44808c22fc0e2f60e04f7a
Stanislau Trebnikau <Stanislau_Trebnikau@epam.com>
2022-05-25 16:39:38 +0300

    authorization procedure, add security-taglibs dependency for tags into views

--- src/main/java/com/trebnikau/configuration/MySecurityConfig.java
+++ src/main/java/com/trebnikau/configuration/MySecurityConfig.java
@@ -1,6 +1,7 @@
 package com.trebnikau.configuration;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -25,5 +26,19 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                         username("ivan").
                         password("ivan").
                         roles("MANAGER","HR"));
+    }
+
+    // Метод для разрешение доступа к URL для конкретных ролей
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().
+                antMatchers("/").hasAnyRole(
+                        "EMPLOYEE",
+                                "HR",
+                                "MANAGER").
+                antMatchers("/hr_info").hasRole("HR").
+                antMatchers("/manager_info").hasRole("MANAGER").
+                // Форма логина и пароля будет запрашиваться у всех
+                and().formLogin().permitAll();
     }
 }
